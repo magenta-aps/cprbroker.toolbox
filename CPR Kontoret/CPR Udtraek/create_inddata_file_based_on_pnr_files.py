4a13e809c9e19a86f648c9a61038fa34fa7124ad
Heini Leander Ovason <heini.ovason@gmail.com>
2018-05-25 14:01:07 +0200

    Improved string formatting. Regex validation of pnrs. Set parsed to sorted list.

--- CPR Kontoret/CPR Udtraek/create_inddata_file_based_on_pnr_files.py
+++ CPR Kontoret/CPR Udtraek/create_inddata_file_based_on_pnr_files.py
@@ -1,10 +1,19 @@
+import re
+
+
+__author__ = "Heini Leander Ovason"
+
 ##############################################
 ##### Creating a Set of pnr from n files #####
 ##############################################
 
+""" NOTE: The example files indicate the expected format.
+They are simulating extracts from CPR Broker' PersonSearchCache,
+and DPR EMulation' DTTOTAL."""
 files = ['exampe_file_01', 'exampe_file_02']
 
-pnrs = set()
+# Solely for avoiding duplicate pnr entries.
+pnr_set = set()
 
 for path in files:
 
@@ -12,15 +21,40 @@ for path in files:
 
     for line in pnr_file:
 
-        # TODO: CHECK FOR 10 digits!
+        check_line = re.match(r'^\d{10}$', line)
 
-        # NOTE: Only grabbing 10 first chars, and leaving out the potential '\n'
-        format_line = line[0:10]
-        pnrs.add(format_line)
+        if check_line:
+            
+            # leave out potential '\n'
+            formatted_line = line[0:10]
+
+            pnr_set.add(formatted_line)
+
+        """If the line is 9 digits, and the file contains person numbers 
+        from a DprEmulering.dbo.DTTOTAL then there will not be any
+        leading zeros. This is because a person number is of type
+        int the DPR data model."""
+        check_for_dpr_format = re.match(r'^\d{9}$', line)
+
+        if check_for_dpr_format:
+
+            # Prepend a zero and leave out potential '\n'
+            formatted_dpr_line = '0{}'.format(line[:9])
+
+            pnr_set.add(formatted_dpr_line)
+        
 
     pnr_file.close()
 
-# TODO: Set must be an ordered list, and the file format = ISO-8859-1
+# Convert to list
+pnr_list = list(pnr_set)
+
+# It is a requirement that the list is naturally sorted.
+pnr_list.sort()
+
+""" NOTE: CPR Udtraek docs. specify that charset should 
+be ISO-8859-1 (ASCII), but this should not matter if we 
+are only using alphanumerical values """
 
 koerselsdato = '180530' # e.g. 180528
 inddatatype = '01'
@@ -31,16 +65,17 @@ filler = '                                               '
 inddata_filename = 'd{}.i{}'.format(koerselsdato, opgavenummer)
 inddata_file = open(inddata_filename, 'a', newline='\n')
 
-for personnr in pnrs:
+for personnr in pnr_list:
 
-    inddata_file.write( 
-        inddatatype + 
-        opgavenummer + 
-        personnr + 
-        noeglekonstant + 
-        filler + 
-        '\n'
-    )
+    inddata_person = '{}{}{}{}{}\n'.format(
+                                            inddatatype,
+                                            opgavenummer, 
+                                            personnr,
+                                            noeglekonstant,
+                                            filler
+                                            )
+
+    inddata_file.write(inddata_person)
 
 inddata_file.close()
 
