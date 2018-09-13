8e7566cc610a0ac01af87a625cc181d48e835cc4
Heini Leander Ovason <heini.ovason@gmail.com>
2018-09-13 10:29:58 +0200

    powershell script for iterating pnr file and calling dpr emulation diversion

---
+++ misc/powershell-scripts/for_each_pnr_in_file_call_dpr_diversion.ps1
@@ -0,0 +1,9 @@
+$dpr_emulering_diversion_host = "<private ip>"
+$dpr_emulering_diversion_port =  "<port>"
+$target_file = Get-Content <file_names_are_referenced_without_quotes>
+$dpr_emulering_diversion = "<C:\PATH\TO\DprDiversionCaller.exe>"
+
+# ampersand is used to run the referenced executable, along with the 3 arguments
+foreach ($cprnr in $target_file) {
+    & $dpr_emulering_diversion $dpr_emulering_diversion_host $dpr_emulering_diversion_port $cprnr
+}
