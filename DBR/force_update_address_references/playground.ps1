bc43997bf6cd9ff8d82a2f7a3c6c3ac23642fe26
Heini Leander Ovason <heini.ovason@gmail.com>
2019-02-18 12:08:14 +0000

    Download dir moved back to settings from code

--- DBR/force_update_address_references/playground.ps1
+++ DBR/force_update_address_references/playground.ps1
@@ -2,9 +2,16 @@
 
 [xml]$ConfigFile = Get-Content "./Settings.xml"
 
-$uriList = @($ConfigFile.Settings.PostDistrictTxtFile, $ConfigFile.Settings.GeoLocationZipFile) 
+New-Variable -Name "DOWNLOAD_FOLDER" -Value $ConfigFile.Settings.DownloadFolder -Option Constant
+$downloadPath = [string]::Format("{0}\{1}", $PSScriptRoot, $DOWNLOAD_FOLDER)
+New-Item -ItemType Directory -Force -Path $downloadPath
+$uriList = @(
+    $ConfigFile.Settings.PostDistrictTxtFile,
+    $ConfigFile.Settings.GeoLocationZipFile
+    ) 
 DownloadPostDistrictAndGeoLocationFiles -uriList $uriList 
 
+#$fileList = Get-ChildItem
 # UPS! Is it necessary to process the post district file first ???
 # --> Check the update methods in BatchClient.
 #
