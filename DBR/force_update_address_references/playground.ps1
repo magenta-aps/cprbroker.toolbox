922bc545e86f639a62d6bd63ff7c72168fd17907
Heini Leander Ovason <heini.ovason@gmail.com>
2019-02-18 11:54:06 +0000

    playground cleanup

--- DBR/force_update_address_references/playground.ps1
+++ DBR/force_update_address_references/playground.ps1
@@ -2,27 +2,8 @@
 
 [xml]$ConfigFile = Get-Content "./Settings.xml"
 
-$downloadFolderPath = ($PSScriptRoot + "\" + $ConfigFile.Settings.DownloadFolderName)
-New-Item -ItemType Directory -Force -Path $downloadFolderPath
-
 $uriList = @($ConfigFile.Settings.PostDistrictTxtFile, $ConfigFile.Settings.GeoLocationZipFile) 
-
-$wc = New-Object System.Net.WebClient
-
-foreach ($uri in $uriList)
-{
-    try {
-        Write-Host ("Trying to download file from " + $uri)
-        $filename = $uri.substring($uri.lastIndexOf('/') + 1);
-        $wc.DownloadFile($uri, ($downloadFolderPath + "\" + $filename))
-    }
-    catch [Net.WebException] 
-    {
-        Write-Host $_.Exception.ToString()
-        Write-Host "Exiting program..."
-        exit
-    }
-}
+DownloadPostDistrictAndGeoLocationFiles -uriList $uriList 
 
 # UPS! Is it necessary to process the post district file first ???
 # --> Check the update methods in BatchClient.
