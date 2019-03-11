98a6cf37a4c118e1beb5d7426622f6e569fc06bc
Heini Leander Ovason <heini.ovason@gmail.com>
2019-03-11 09:07:24 +0100

    Minor correction

--- misc/powershell-scripts/for_each_pnr_in_file_call_dpr_diversion.ps1
+++ misc/powershell-scripts/for_each_pnr_in_file_call_dpr_diversion.ps1
@@ -1,6 +1,6 @@
 $dpr_emulering_diversion_host = "<private ip>"
 $dpr_emulering_diversion_port =  "<port>"
-$target_file = Get-Content <file_names_are_referenced_without_quotes>
+$target_file = Get-Content "<file_names_are_referenced_without_quotes>"
 $dpr_emulering_diversion = "<C:\PATH\TO\DprDiversionCaller.exe>"
 
 # ampersand is used to run the referenced executable, along with the 3 arguments
