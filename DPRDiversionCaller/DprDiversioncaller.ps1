1c07a434078f475b01ef28ddb7fe4c9caef6a76a
heiniovason <heini.ovason@gmail.com>
2021-09-07 13:20:39 +0200

    modified

--- DPRDiversionCaller/DprDiversioncaller.ps1
+++ DPRDiversionCaller/DprDiversioncaller.ps1
@@ -2,34 +2,31 @@
 # Returns cpr data in the form CPR Direct proxy sends it
 
 # Consider changing to commandline arguments
-$server = "10.0.2.15"
-$port = 53687
+$server = "<IP_ADDRESS>"
+$port = <PORT_NO>
+$cpr_no "CPR_NO"
 
-$pnrFile = Get-Content ""
-
-$inquiryType = "1"
+##### INQUIRY TYPES #####
 # * DataNotUpdatedAutomatically = 0
 # * DataUpdatedAutomaticallyFromCpr = 1
 # * DeleteAutomaticDataUpdateFromCpr = 3
+$inquiryType = "1"
 
-$detailType = "1"
+##### DETAIL TYPES #####
 # * MasterData = 0, // Only to client
 # * ExtendedData = 1 // Put to DPR database
+$detailType = "1"
 
-# 10 digit cpr number, aka person registration number
+$message = ($inquiryType + $detailType + $cpr_no)
 
-$message = ($inquiryType + $detailType + $cprno)
-
-$requestInfo = [string]::Format("Request attempt to {0}:{1}", $server, $port)
+$requestInfo = [string]::Format("Request attempt to {0}:{1} with message: {2}", $server, $port, $message)
 Write-Output $requestInfo
 
-foreach ($cprnr in $target_file)
-{
-    $socket = new-object System.Net.Sockets.TcpClient($server, $port)
-    $data = [System.Text.Encoding]::ASCII.GetBytes($message)
-    $stream = $socket.GetStream()
-    $stream.Write($data, 0, $data.Length)
-    # Read response? See : https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.networkstream.read?view=netframework-4.7.2
-    $stream.Close()
-    $socket.Close()
-}
+$socket = new-object System.Net.Sockets.TcpClient($server, $port)
+$data = [System.Text.Encoding]::ASCII.GetBytes($message)
+$stream = $socket.GetStream()
+$stream.Write($data, 0, $data.Length)
+# Reading response from stream missing !
+# Read response? See : https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.networkstream.read?view=netframework-4.7.2
+$stream.Close()
+$socket.Close()
